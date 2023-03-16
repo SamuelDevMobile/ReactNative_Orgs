a2e1550e2e3659ee64e7872ab9fafd8b445239fb
Samuel Ribeiro <samuelribeiro9@icloud.com>
2023-03-16 14:01:13 -0300

    adicionado fonts do google

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
-import { StatusBar, SafeAreaView } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
 import Cesta from './src/screens/Cesta';
+import { 
+    useFonts,
+    Montserrat_400Regular,
+    Montserrat_700Bold
+   } from '@expo-google-fonts/montserrat';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <View />;
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
