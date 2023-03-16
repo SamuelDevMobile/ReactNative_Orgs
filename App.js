bafe0550b5225dd977e8fd25a3406bd4e8ef6328
Samuel Ribeiro <samuelribeiro9@icloud.com>
2023-03-16 15:26:02 -0300

    refatoracao e adicionado mocks

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import {
     Montserrat_400Regular,
     Montserrat_700Bold
    } from '@expo-google-fonts/montserrat';
+   
+   import mock from './src/mocks/cesta'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,7 +21,7 @@ export default function App() {
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock} />
     </SafeAreaView>
   );
 }
---
+++ src/screens/Cesta/index.js
@@ -0,0 +1,20 @@
+import { StyleSheet, View } from "react-native"
+import Detalhes from "./components/Detalhes"
+import Topo from "./components/Topo"
+
+export default function Cesta({ topo, detalhes}) {
+    return <>
+        <Topo {...topo} />
+    
+        <View style={styles.cesta}>
+            <Detalhes {...detalhes} />
+        </View>
+    </>
+}
+
+const styles = StyleSheet.create({
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+})
